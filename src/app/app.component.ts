c23ed637d1630b8a97bc1d1d045d10b527758226
lakshmi narayana <lakshminarayanaakki@gmail.com>
2019-04-10 23:11:49 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { User } from './User';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'userLogin';
+
+  userInformation:User[]=[];
+  addUser(form){
+   console.log(form.value);
+  }
+
+
+  
 }
